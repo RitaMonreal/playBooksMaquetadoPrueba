66ff0a290762c30b2d2886bfd4e6052f9392797d
RitaMonreal <A326347@alumnos.uaslp.mx>
2023-03-05 13:12:36 -0600

    funciona la incersión en columna de list view horizontal

---
+++ android/app/src/main/kotlin/com/example/maquetado_playbooks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.maquetado_playbooks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+            children: [
+              Expanded(
+                child: ListView(
+                  // This next line does the trick.
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                      width: 160.0,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      width: 160.0,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      width: 160.0,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+              ),
+            ]
+        ),
+      ),
+    );
+  }
+}
