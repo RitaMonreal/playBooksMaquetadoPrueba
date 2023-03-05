f4cc57f1e29ba5852650a1b8ac4fd17f59dce262
RitaMonreal <A326347@alumnos.uaslp.mx>
2023-03-05 17:06:34 -0600

    Estructura del layout a la mitad, funciona el listview, appbar está modificado con el tamaño

--- lib/main.dart
+++ lib/main.dart
@@ -7,34 +7,67 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const title = 'Search Play Books';
 
     return MaterialApp(
+      title: title,
       home: Scaffold(
+        appBar:_buildMainAppBar(),
         body: Column(
-            children: [
-              Expanded(
-                child: ListView(
-                  // This next line does the trick.
-                  scrollDirection: Axis.horizontal,
-                  children: <Widget>[
-                    Container(
-                      width: 160.0,
-                      color: Colors.red,
-                    ),
-                    Container(
-                      width: 160.0,
-                      color: Colors.blue,
-                    ),
-                    Container(
-                      width: 160.0,
-                      color: Colors.green,
-                    ),
-                  ],
-                ),
+          children: <Widget>[
+            Container(
+              //margin: const EdgeInsets.symmetric(vertical: 15.0),
+              margin: const EdgeInsets.only(top: 15.0),
+              color: Colors.black54,
+              height: 300.0,
+              child: ListView(
+                // This next line does the trick.
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    width: 170.0,
+                    margin: const EdgeInsets.all(3.0),
+                    color: Colors.red,
+                  ),
+                  Container(
+                    width: 170.0,
+                    margin: const EdgeInsets.all(3.0),
+                    color: Colors.black38,
+                  ),
+                  Container(
+                    width: 170.0,
+                    margin: const EdgeInsets.all(3.0),
+                    color: Colors.amberAccent,
+                  ),
+                  Container(
+                    width: 170.0,
+                    height: 270.0,
+                    margin: const EdgeInsets.all(3.0),
+                    color: Colors.greenAccent,
+                  ),
+                ],
               ),
-            ]
+            ),
+            FractionallySizedBox(
+              alignment: Alignment.topCenter,
+              widthFactor: 1.0,
+              //heightFactor: 1.0,
+              child: Container(
+                color: Colors.cyan,
+                height: 456,
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+AppBar _buildMainAppBar() {
+  return AppBar(
+      leading: Icon(Icons.search),
+      backgroundColor: Colors.black45,
+      title: const Text('Search Play Books')
+  );
 }
