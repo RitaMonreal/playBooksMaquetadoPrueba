b973374354b4cceeb8a82dbd3b3581a535e6a64a
RitaMonreal <A326347@alumnos.uaslp.mx>
2023-03-06 19:21:58 -0600

    se agregó tapbar inferior y se recortó código, se va a quitar el appbar

--- lib/main.dart
+++ lib/main.dart
@@ -14,57 +14,68 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
           backgroundColor: const Color.fromRGBO(38, 36, 36, 1.0),
         appBar:_buildMainAppBar(),
-        body: Column(
-          children: <Widget>[
-            Container(
-              //margin: const EdgeInsets.symmetric(vertical: 15.0),
-              margin: const EdgeInsets.only(top: 15.0),
-              //color: Colors.black54,
-              height: 300.0,
-              child: ListView(
-                // This next line does the trick.
-                scrollDirection: Axis.horizontal,
-                children: <Widget>[
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                //margin: const EdgeInsets.symmetric(vertical: 15.0),
+                margin: const EdgeInsets.only(top: 15.0),
+                //color: Colors.black54,
+                height: 300.0,
+                child: ListView(
+                  // This next line does the trick.
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    Container(
+                        width: 170.0,
+                        margin: const EdgeInsets.all(3.0),
+                        color: Colors.red,
+                    ),
+                    Container(
+                      width: 170.0,
+                      margin: const EdgeInsets.all(3.0),
+                      color: Colors.black38,
+                    ),
+                    Container(
+                      width: 170.0,
+                      margin: const EdgeInsets.all(3.0),
+                      color: Colors.amberAccent,
+                    ),
+                    Container(
+                      width: 170.0,
+                      height: 270.0,
+                      margin: const EdgeInsets.all(3.0),
+                      color: Colors.greenAccent,
+                    ),
+                  ],
+                ),
+              ),
+
+              //Stack(
+                //children:[
                   Container(
-                    width: 170.0,
-                    margin: const EdgeInsets.all(3.0),
-                    color: Colors.red,
-                  ),
-                  Container(
-                    width: 170.0,
-                    margin: const EdgeInsets.all(3.0),
-                    color: Colors.black38,
-                  ),
-                  Container(
-                    width: 170.0,
-                    margin: const EdgeInsets.all(3.0),
-                    color: Colors.amberAccent,
-                  ),
-                  Container(
-                    width: 170.0,
-                    height: 270.0,
-                    margin: const EdgeInsets.all(3.0),
+                    alignment: Alignment.topCenter,
                     color: Colors.greenAccent,
+                    height: 400,
+                    child:  _tabTiposLibros(context),
+
                   ),
-                ],
-              ),
-            ),
-            FractionallySizedBox(
-              alignment: Alignment.topCenter,
-              widthFactor: 1.0,
-              //heightFactor: 1.0,
-              child: Container(
-                color: Color.fromRGBO(38, 36, 36, 0.5),
-                height: 456,
-                child:  _tabTiposLibros(context),
-              ),
-            ),
-          ],
-        ),
+                  Container(
+
+                    color: Colors.yellow,
+                    child:  _tabMenuInferior(context),
+                  )
+               // ]
+             // )
+            ],
+          ),
+        )
       ),
     );
   }
 }
+
+//
 
 AppBar _buildMainAppBar() {
   return AppBar(
@@ -73,6 +84,29 @@ AppBar _buildMainAppBar() {
       title: const Text('Search Play Books')
   );
 }
+
+Widget _tabMenuInferior(BuildContext context) {
+  return  DefaultTabController(
+
+    length: 4, // length of tabs
+    child: Column(crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: const <Widget>[
+      TabBar(
+        indicatorColor: Colors.transparent,
+        labelColor: Colors.lightBlue,
+        unselectedLabelColor: Colors.grey,
+        tabs: [
+          Tab(text: 'Home', icon: Icon(Icons.home_outlined)),
+          Tab(text: 'Library', icon: Icon(Icons.library_books_outlined)),
+          Tab(text: 'Wishlist', icon: Icon(Icons.bookmark_border_outlined)),
+          Tab(text: 'Shop', icon:Icon(Icons.store_outlined)),
+        ],
+      ),
+    ],
+    ),
+  );
+}
+
 
 
 Widget _tabTiposLibros(BuildContext context) {
