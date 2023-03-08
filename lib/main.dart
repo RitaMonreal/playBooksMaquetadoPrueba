73689b5d6b2af8a7ebf953ffe8e964d8ae1ce8e2
RitaMonreal <A326347@alumnos.uaslp.mx>
2023-03-08 13:10:47 -0600

    Ya se separó el contenedor que tiene menú inferior de la columna de desplazamiento, pero ahora no hay desplazamiento :(((

--- lib/main.dart
+++ lib/main.dart
@@ -12,77 +12,77 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: title,
       home: Scaffold(
-          backgroundColor: Colors.pink,
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
+        backgroundColor: Colors.pink,
+        //Columna que abarca toda la pantalla
+        body:Column(
+          children: <Widget>[
+            //Se aplica solo a la parte móvil de la pantalla
+            SingleChildScrollView(
+              //Columna que abarca toda la pantalla menos el menú inferior
+              child: Column(
+                children: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.only(left: 8.0, top: 45.0,right: 8.0, bottom: 0.0 ),
 
-              const Padding(
-                  padding: EdgeInsets.only(left: 8.0, top: 45.0,right: 8.0, bottom: 0.0 ),
-
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      filled: true,
-                      fillColor: Colors.white,
-                      hintText: 'Search Play Books',
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'Search Play Books',
+                      ),
                     ),
-
                   ),
-                ),
-              Container(
-                //margin: const EdgeInsets.symmetric(vertical: 15.0),
-                margin: const EdgeInsets.only(top: 15.0),
-                //color: Colors.black54,
-                height: 300.0,
-                child: ListView(
-                  // This next line does the trick.
-                  scrollDirection: Axis.horizontal,
-                  children: <Widget>[
-                    Container(
-                        width: 170.0,
-                        margin: const EdgeInsets.all(3.0),
-                        color: Colors.red,
+                  Container(
+                    //margin: const EdgeInsets.symmetric(vertical: 15.0),
+                    margin: const EdgeInsets.only(top: 15.0),
+                    //color: Colors.black54,
+                    height: 300.0,
+                    child: ListView(
+                      // This next line does the trick.
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        Container(
+                          width: 170.0,
+                          margin: const EdgeInsets.all(3.0),
+                          color: Colors.red,
+                        ),
+                        Container(
+                          width: 170.0,
+                          margin: const EdgeInsets.all(3.0),
+                          color: Colors.black38,
+                        ),
+                        Container(
+                          width: 170.0,
+                          margin: const EdgeInsets.all(3.0),
+                          color: Colors.amberAccent,
+                        ),
+                        Container(
+                          width: 170.0,
+                          height: 270.0,
+                          margin: const EdgeInsets.all(3.0),
+                          color: Colors.greenAccent,
+                        ),
+                      ],
                     ),
-                    Container(
-                      width: 170.0,
-                      margin: const EdgeInsets.all(3.0),
-                      color: Colors.black38,
-                    ),
-                    Container(
-                      width: 170.0,
-                      margin: const EdgeInsets.all(3.0),
-                      color: Colors.amberAccent,
-                    ),
-                    Container(
-                      width: 170.0,
-                      height: 270.0,
-                      margin: const EdgeInsets.all(3.0),
-                      color: Colors.greenAccent,
-                    ),
-                  ],
-                ),
-              ),
-
-              //Stack(
-                //children:[
+                  ),
                   Container(
                     alignment: Alignment.topCenter,
                     color: Colors.greenAccent,
-                    height: 300,
+                    height: 368,
                     child:  _tabTiposLibros(context),
 
                   ),
-                  Container(
-
-                    color: Colors.yellow,
-                    child:  _tabMenuInferior(context),
-                  )
-               // ]
-             // )
-            ],
-          ),
-        )
+                ],
+              ),
+            ),
+            Container(
+              //alignment: Alignment.topCenter,
+              color: Colors.yellow,
+              child:  _tabMenuInferior(context),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -95,18 +95,18 @@ Widget _tabMenuInferior(BuildContext context) {
     length: 4, // length of tabs
     child: Column(crossAxisAlignment: CrossAxisAlignment.stretch,
       children: const <Widget>[
-      TabBar(
-        indicatorColor: Colors.transparent,
-        labelColor: Colors.lightBlue,
-        unselectedLabelColor: Colors.grey,
-        tabs: [
-          Tab(text: 'Home', icon: Icon(Icons.home_outlined)),
-          Tab(text: 'Library', icon: Icon(Icons.library_books_outlined)),
-          Tab(text: 'Wishlist', icon: Icon(Icons.bookmark_border_outlined)),
-          Tab(text: 'Shop', icon:Icon(Icons.store_outlined)),
-        ],
-      ),
-    ],
+        TabBar(
+          indicatorColor: Colors.transparent,
+          labelColor: Colors.lightBlue,
+          unselectedLabelColor: Colors.grey,
+          tabs: [
+            Tab(text: 'Home', icon: Icon(Icons.home_outlined)),
+            Tab(text: 'Library', icon: Icon(Icons.library_books_outlined)),
+            Tab(text: 'Wishlist', icon: Icon(Icons.bookmark_border_outlined)),
+            Tab(text: 'Shop', icon:Icon(Icons.store_outlined)),
+          ],
+        ),
+      ],
     ),
   );
 }
