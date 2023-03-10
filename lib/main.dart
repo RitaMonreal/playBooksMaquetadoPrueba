98a403e11f01af232a5b6ffe413689391a28be00
RitaMonreal <A326347@alumnos.uaslp.mx>
2023-03-09 22:04:03 -0600

    no sé que hice aquí, pero es la version fisicamente más bonita, se modifica código para que no todo este en el main :))

--- lib/main.dart
+++ lib/main.dart
@@ -1,151 +1,150 @@
 import 'package:flutter/material.dart';
-
 void main() => runApp(const MyApp());
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     const title = 'Search Play Books';
-
     return MaterialApp(
       title: title,
       home: Scaffold(
-        backgroundColor: Colors.pink,
-        //Columna que abarca toda la pantalla
+        backgroundColor: Colors.grey.shade900,
+//Columna que abarca toda la pantalla
         body:Column(
           children: <Widget>[
-            //Se aplica solo a la parte móvil de la pantalla
-            SingleChildScrollView(
-              //Columna que abarca toda la pantalla menos el menú inferior
-              child: Column(
-                children: <Widget>[
-                  const Padding(
-                    padding: EdgeInsets.only(left: 8.0, top: 45.0,right: 8.0, bottom: 0.0 ),
+//Se aplica solo a la parte móvil de la pantalla
+            Expanded(
+              child:  SingleChildScrollView(
+//Columna que abarca toda la pantalla menos el menú inferior
+                child: Column(
+                  children: <Widget>[
+                    _searchBox(context),
+                    //Contenedor de la list view
+                    Container(
+                      margin: const EdgeInsets.only(top: 13.0, left: 15.0, right: 15.0),
+                      height: 310.0,
+                      child: ListView(
+// This next line does the trick.
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+//Container de toda la información hereda el tamaño de altura que dí para toda la listView
+                          Container(
+                            width: 170.0,
+                            margin: const EdgeInsets.all(4.0),
+                            child: Column(
+                                children: <Widget>[
+                                  Container(
+                                    height: 250.0,
+                                    decoration: const BoxDecoration(
+                                      image:  DecorationImage(
+                                        image: ExactAssetImage('assets/littleLife.jpg'),
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                  ),
+                                  const Text ('Little Life',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(fontSize: 15,
+                                          color: Colors.white)),
+                                  const Text('100% complete',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(color: Colors.grey)),
+                                ]
+                            ),
+                          ),
 
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        filled: true,
-                        fillColor: Colors.white,
-                        hintText: 'Search Play Books',
+                          Container(
+                            width: 170.0,
+                            margin: const EdgeInsets.all(4.0),
+                            child: Column(
+                                children: <Widget>[
+                                  Container(
+                                    height: 250.0,
+                                    decoration: const BoxDecoration(
+                                      image: DecorationImage(
+                                        image: ExactAssetImage('assets/janeEyre.jpg'),
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                  ),
+                                  const Text ('Jane Eyre',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(fontSize: 15,
+                                          color: Colors.white)
+                                  ),
+                                  const Text('64% complete',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(color: Colors.grey)),
+                                ]
+                            ),
+                          ),
+                          Container(
+                            width: 170.0,
+                            margin: const EdgeInsets.all(4.0),
+                            child: Column(
+                                children: <Widget>[
+                                  Container(
+                                    height: 250.0,
+                                    decoration: const BoxDecoration(
+                                      image: DecorationImage(
+                                        image: ExactAssetImage('assets/cruelPrince.jpg'),
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                  ),
+                                  const Text ('The Cruel Prince',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(fontSize: 15,
+                                      color: Colors.white)),
+                                  const Text('3% complete',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(color: Colors.grey)),
+                                ]
+                            ),
+                          ),
+                          Container(
+                            width: 170.0,
+                            margin: const EdgeInsets.all(4.0),
+                            child: Column(
+                                children: <Widget>[
+                                  Container(
+                                    height: 250.0,
+                                    decoration: const BoxDecoration(
+                                      image: DecorationImage(
+                                        image: ExactAssetImage('assets/tokioBlues.jpg'),
+                                        fit: BoxFit.fill,
+                                      ),
+                                    ),
+                                  ),
+                                  const Text ('Tokio blues',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(fontSize: 15,
+                                          color: Colors.white)),
+                                  const Text('0% complete',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(color: Colors.grey))
+                                ]
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
-                  //Contenedor de la list view
-                  Container(
-                    //margin: const EdgeInsets.symmetric(vertical: 15.0),
-                    margin: const EdgeInsets.only(top: 11.0),
-                    //color: Colors.black54,
-                    height: 300.0,
-                    child: ListView(
-                      // This next line does the trick.
-                      scrollDirection: Axis.horizontal,
-                      children: <Widget>[
-                        //Container de toda la información hereda el tamaño de altura que dí para toda la listView
-                        Container(
-                          width: 170.0,
-                          margin: const EdgeInsets.all(3.0),
-                          child: Column(
-                              children: <Widget>[
-                                Container(
-                                  height: 250.0,
-                                  decoration: new BoxDecoration(
-                                    image: new DecorationImage(
-                                      image: ExactAssetImage('assets/littleLife.jpg'),
-                                      fit: BoxFit.fill,
-                                    ),
-                                  ),
-                                ),
-                                Text ('Little Life', textAlign: TextAlign.center),
-                                Text('100% complete', textAlign: TextAlign.center)
-                                ]
-                          ),
-                        ),
-                        Container(
-                          width: 170.0,
-                          margin: const EdgeInsets.all(3.0),
-                          child: Column(
-                              children: <Widget>[
-                                Container(
-                                  height: 250.0,
-                                  decoration: new BoxDecoration(
-                                    image: new DecorationImage(
-                                      image: ExactAssetImage('assets/janeEyre.jpg'),
-                                      fit: BoxFit.fill,
-                                    ),
-                                  ),
-                                ),
-                                Text ('Jane Eyre', textAlign: TextAlign.center),
-                                Text('64% complete', textAlign: TextAlign.center)
-                              ]
-                          ),
-                        ),
-                        Container(
-                          width: 170.0,
-                          margin: const EdgeInsets.all(3.0),
-                          child: Column(
-                              children: <Widget>[
-                                Container(
-                                  height: 250.0,
-                                  decoration: new BoxDecoration(
-                                    image: new DecorationImage(
-                                      image: ExactAssetImage('assets/cruelPrince.jpg'),
-                                      fit: BoxFit.fill,
-                                    ),
-                                  ),
-                                ),
-                                Text ('The Cruel Prince', textAlign: TextAlign.center),
-                                Text('3% complete', textAlign: TextAlign.center)
-                              ]
-                          ),
-                        ),
-                        Container(
-                          width: 170.0,
-                          margin: const EdgeInsets.all(3.0),
-                          child: Column(
-                              children: <Widget>[
-                                Container(
-                                  height: 250.0,
-                                  decoration: new BoxDecoration(
-                                    image: new DecorationImage(
-                                      image: ExactAssetImage('assets/tokioBlues.jpg'),
-                                      fit: BoxFit.fill,
-                                    ),
-                                  ),
-                                ),
-                                Text ('Tokio blues', textAlign: TextAlign.center),
-                                Text('0% complete', textAlign: TextAlign.center)
-                              ]
-                          ),
-                        ),
-                      ],
+//tabBar a mitad de pantalla
+                    DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade800,
+                        border: const Border(bottom: BorderSide(color: Colors.grey, width: 0.5)),
+                      ),
+                      child:  _tabTiposLibros(context),
                     ),
-                  ),
-                  //tabBar a mitad de pantalla
-                  Container(
-
-                    alignment: Alignment.topCenter,
-                    color: Colors.greenAccent,
-                    height: 368,
-
-                    child: Column(
-                      children: <Widget>[
-                        _tabTiposLibros(context),
-                        Container(
-                          height: 700.0,
-                          color: Colors.blue,
-                        )
-                      ],
-                    )
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             Container(
-              //alignment: Alignment.topCenter,
-              color: Colors.yellow,
+//alignment: Alignment.topCenter,
+              color: Colors.grey.shade800,
+              height: 74.0,
               child:  _tabMenuInferior(context),
             ),
           ],
@@ -158,12 +157,12 @@ class MyApp extends StatelessWidget {
 
 Widget _tabMenuInferior(BuildContext context) {
   return  DefaultTabController(
-
     length: 4, // length of tabs
     child: Column(crossAxisAlignment: CrossAxisAlignment.stretch,
       children: const <Widget>[
         TabBar(
           indicatorColor: Colors.transparent,
+
           labelColor: Colors.lightBlue,
           unselectedLabelColor: Colors.grey,
           tabs: [
@@ -178,15 +177,33 @@ Widget _tabMenuInferior(BuildContext context) {
   );
 }
 
-
+Widget _searchBox(BuildContext context){
+  return const Padding(
+    padding: EdgeInsets.only(left: 8.0, top: 45.0,right: 8.0, bottom: 0.0 ),
+    child: TextField(
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.search),
+        border: OutlineInputBorder(),
+        filled: true,
+        fillColor: Colors.grey,
+        contentPadding: EdgeInsets.symmetric(vertical: 12),
+        hintText: 'Search Play Books',
+      ),
+    ),
+  );
+}
 
 Widget _tabTiposLibros(BuildContext context) {
-  return  DefaultTabController(
+  return DefaultTabController(
     length: 2, // length of tabs
-    child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: const <Widget>[
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch, children: const <Widget>[
       TabBar(
+        indicatorColor: Colors.lightBlue,
+        indicatorSize: TabBarIndicatorSize.label,
         labelColor: Colors.lightBlue,
         unselectedLabelColor: Colors.grey,
+        labelPadding: EdgeInsets.all(0),
         tabs: [
           Tab(text: 'Ebooks'),
           Tab(text: 'AudioBooks'),
@@ -196,3 +213,4 @@ Widget _tabTiposLibros(BuildContext context) {
     ),
   );
 }
+
